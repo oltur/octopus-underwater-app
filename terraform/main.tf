f8c4c91e52afe2413405cf89b4f37599fb97a273
Alexander Turevskiy <3716384+oltur@users.noreply.github.com>
2023-03-05 23:14:36 +0100

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,12 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
+provider "kubernetes" {
+  config_path    = "~/.kube/config"
+  config_context = "arn:aws:eks:eu-west-1:916438688338:cluster/terraform-cluster-1"
+  # load_config_file = false
+}
+
 # data "aws_ami" "app_ami" {
 #   most_recent = true
 #   owners      = ["amazon"]
@@ -39,62 +45,117 @@ resource "aws_ecr_repository" "ecr-oltur-terraform" {
   # }
 }
 
-# resource "kubernetes_deployment" "example" {
-#   metadata {
-#     name = "terraform-example"
-#     labels = {
-#       test = "MyExampleApp"
-#     }
-#   }
+resource "aws_eks_cluster" "terraform-cluster-1" {
+    name     = "terraform-cluster-1"
+    enabled_cluster_log_types = []
+    role_arn                  = "arn:aws:iam::916438688338:role/eksctl-terraform-cluster-1-cluster-ServiceRole-74ADPB8PZOC6"
+    tags                      = {
+        "Name"                                        = "eksctl-terraform-cluster-1-cluster/ControlPlane"
+        "alpha.eksctl.io/cluster-name"                = "terraform-cluster-1"
+        "alpha.eksctl.io/cluster-oidc-enabled"        = "true"
+        "alpha.eksctl.io/eksctl-version"              = "0.132.0"
+        "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "terraform-cluster-1"
+    }
+    tags_all                  = {
+        "Name"                                        = "eksctl-terraform-cluster-1-cluster/ControlPlane"
+        "alpha.eksctl.io/cluster-name"                = "terraform-cluster-1"
+        "alpha.eksctl.io/cluster-oidc-enabled"        = "true"
+        "alpha.eksctl.io/eksctl-version"              = "0.132.0"
+        "eksctl.cluster.k8s.io/v1alpha1/cluster-name" = "terraform-cluster-1"
+    }
+    version                   = "1.24"
 
-#   spec {
-#     replicas = 3
+    kubernetes_network_config {
+        ip_family         = "ipv4"
+        service_ipv4_cidr = "10.100.0.0/16"
+    }
 
-#     selector {
-#       match_labels = {
-#         test = "MyExampleApp"
-#       }
-#     }
+    vpc_config {
+        endpoint_private_access   = false
+        endpoint_public_access    = true
+        public_access_cidrs       = [
+            "0.0.0.0/0",
+        ]
+        security_group_ids        = [
+            "sg-0fd7d77abf67df98e",
+        ]
+        subnet_ids                = [
+            "subnet-01e3cb63d5695a5bf",
+            "subnet-02173240493284119",
+            "subnet-04d56f5157ed685f3",
+            "subnet-04e6709882e1433c4",
+            "subnet-066d1a420c6af419d",
+            "subnet-0c4854552324f79f8",
+        ]
+    }
+}
 
-#     template {
-#       metadata {
-#         labels = {
-#           test = "MyExampleApp"
-#         }
-#       }
+resource "kubernetes_deployment" "deployment-oltur-terraform" {
+  metadata {
+    name = "oltur-terraform"
+    labels = {
+      app = "oltur-terraform"
+    }
+  }
 
-#       spec {
-#         container {
-#           image = "nginx:1.21.6"
-#           name  = "example"
+  spec {
+    replicas = 2
 
-#           resources {
-#             limits = {
-#               cpu    = "0.5"
-#               memory = "512Mi"
-#             }
-#             requests = {
-#               cpu    = "250m"
-#               memory = "50Mi"
-#             }
-#           }
+    selector {
+      match_labels = {
+        app = "oltur-terraform"
+      }
+    }
 
-#           liveness_probe {
-#             http_get {
-#               path = "/"
-#               port = 80
+    strategy {
+      type = "RollingUpdate"
+    }
 
-#               http_header {
-#                 name  = "X-Custom-Header"
-#                 value = "Awesome"
-#               }
-#             }
+    template {
+      metadata {
+        labels = {
+          app = "oltur-terraform"
+        }
+      }
 
-#             initial_delay_seconds = 3
-#             period_seconds        = 3
-#           }
-#         }
-#       }
-#     }
-#   }
-# }
+      spec {
+        container {
+          image = "916438688338.dkr.ecr.eu-west-1.amazonaws.com/oltur-terraform:latest"
+          name  = "oltur-terraform"
+
+          image_pull_policy = "Always"
+          port {
+            container_port = 80
+            protocol = "TCP"
+          }
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/"
+              port = 80
+
+              http_header {
+                name  = "X-Custom-Header"
+                value = "livez"
+              }
+            }
+
+            initial_delay_seconds = 3
+            period_seconds        = 3
+          }
+        }
+      }
+    }
+  }
+}
